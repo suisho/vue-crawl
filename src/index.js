324febe96cc474c143b8a24bd85af6b217e2d168
suisho <suisho000000@gmail.com>
2014-07-24 00:33:18 +0900

    add text and html crawling

--- src/index.js
+++ src/index.js
@@ -2,21 +2,26 @@
 
 exports.install = function(Vue, options){
   var textDirective = Vue.require("directives").text
+  var htmlDirective = Vue.require("directives").html
+  
   Vue.directive("crawl-text", {
     bind : function(){
       var attr = this.el.nodeType === 3
           ? 'nodeValue'
           : 'textContent'
-      var val = this.vm.$data[this.key] 
-      
-      if(val === undefined){
-      }else{
-        val = [val]
-      }
       this.vm.$data[this.key] = this.el[attr]
     },
     update : function(value){
-      textDirective.update(value)
+      textDirective.update.call(this, value)
+    }
+  })
+  Vue.directive("crawl-html", {
+    bind : function(){
+      var val = this.el.innerHTML
+      this.vm.$data[this.key] = val
+    },
+    update : function(value){
+      return htmlDirective.update.call(this, value)
     }
   })
 }
